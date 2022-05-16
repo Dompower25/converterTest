f461e9fc2edd035c2b29ff4286b1302a29e6f081
Dompower25 <89357747+Dompower25@users.noreply.github.com>
2022-05-16 19:34:21 +0300

    ad

---
+++ js/main.js
@@ -0,0 +1,474 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/modules/old_Cours.js":
+/*!*********************************!*\
+  !*** ./js/modules/old_Cours.js ***!
+  \*********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "oldCoursArr": function() { return /* binding */ oldCoursArr; }
+/* harmony export */ });
+let oldCoursArr = [{
+  Cur_ID: 440,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "AUD",
+  Cur_Scale: 1,
+  Cur_Name: "Австралийский доллар",
+  Cur_OfficialRate: 2.2036
+}, {
+  Cur_ID: 510,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "AMD",
+  Cur_Scale: 1000,
+  Cur_Name: "Армянских драмов",
+  Cur_OfficialRate: 6.0797
+}, {
+  Cur_ID: 441,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "BGN",
+  Cur_Scale: 1,
+  Cur_Name: "Болгарский лев",
+  Cur_OfficialRate: 1.6722
+}, {
+  Cur_ID: 449,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "UAH",
+  Cur_Scale: 100,
+  Cur_Name: "Гривен",
+  Cur_OfficialRate: 9.9685
+}, {
+  Cur_ID: 450,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "DKK",
+  Cur_Scale: 10,
+  Cur_Name: "Датских крон",
+  Cur_OfficialRate: 4.3972
+}, {
+  Cur_ID: 431,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "USD",
+  Cur_Scale: 1,
+  Cur_Name: "Доллар США",
+  Cur_OfficialRate: 2.9432
+}, {
+  Cur_ID: 451,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "EUR",
+  Cur_Scale: 1,
+  Cur_Name: "Евро",
+  Cur_OfficialRate: 3.281
+}, {
+  Cur_ID: 452,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "PLN",
+  Cur_Scale: 10,
+  Cur_Name: "Злотых",
+  Cur_OfficialRate: 7.0284
+}, {
+  Cur_ID: 508,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "JPY",
+  Cur_Scale: 100,
+  Cur_Name: "Иен",
+  Cur_OfficialRate: 2.4141
+}, {
+  Cur_ID: 461,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "IRR",
+  Cur_Scale: 100000,
+  Cur_Name: "Иранских риалов",
+  Cur_OfficialRate: 7.0076
+}, {
+  Cur_ID: 453,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "ISK",
+  Cur_Scale: 100,
+  Cur_Name: "Исландских крон",
+  Cur_OfficialRate: 2.3032
+}, {
+  Cur_ID: 371,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "CAD",
+  Cur_Scale: 1,
+  Cur_Name: "Канадский доллар",
+  Cur_OfficialRate: 2.3491
+}, {
+  Cur_ID: 462,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "CNY",
+  Cur_Scale: 10,
+  Cur_Name: "Китайских юаней",
+  Cur_OfficialRate: 4.641
+}, {
+  Cur_ID: 394,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "KWD",
+  Cur_Scale: 1,
+  Cur_Name: "Кувейтский динар",
+  Cur_OfficialRate: 9.6838
+}, {
+  Cur_ID: 454,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "MDL",
+  Cur_Scale: 10,
+  Cur_Name: "Молдавских леев",
+  Cur_OfficialRate: 1.6057
+}, {
+  Cur_ID: 448,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "NZD",
+  Cur_Scale: 1,
+  Cur_Name: "Новозеландский доллар",
+  Cur_OfficialRate: 2.0414
+}, {
+  Cur_ID: 455,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "NOK",
+  Cur_Scale: 10,
+  Cur_Name: "Норвежских крон",
+  Cur_OfficialRate: 3.3756
+}, {
+  Cur_ID: 456,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "RUB",
+  Cur_Scale: 100,
+  Cur_Name: "Российских рублей",
+  Cur_OfficialRate: 3.5325
+}, {
+  Cur_ID: 457,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "XDR",
+  Cur_Scale: 1,
+  Cur_Name: "СДР (Специальные права заимствования)",
+  Cur_OfficialRate: 4.0713
+}, {
+  Cur_ID: 421,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "SGD",
+  Cur_Scale: 1,
+  Cur_Name: "Сингапурcкий доллар",
+  Cur_OfficialRate: 2.1735
+}, {
+  Cur_ID: 458,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "KGS",
+  Cur_Scale: 100,
+  Cur_Name: "Сомов",
+  Cur_OfficialRate: 3.5317
+}, {
+  Cur_ID: 459,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "KZT",
+  Cur_Scale: 1000,
+  Cur_Name: "Тенге",
+  Cur_OfficialRate: 6.2667
+}, {
+  Cur_ID: 460,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "TRY",
+  Cur_Scale: 10,
+  Cur_Name: "Турецких лир",
+  Cur_OfficialRate: 2.0065
+}, {
+  Cur_ID: 429,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "GBP",
+  Cur_Scale: 1,
+  Cur_Name: "Фунт стерлингов",
+  Cur_OfficialRate: 3.8625
+}, {
+  Cur_ID: 463,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "CZK",
+  Cur_Scale: 100,
+  Cur_Name: "Чешских крон",
+  Cur_OfficialRate: 13.3748
+}, {
+  Cur_ID: 464,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "SEK",
+  Cur_Scale: 10,
+  Cur_Name: "Шведских крон",
+  Cur_OfficialRate: 3.1609
+}, {
+  Cur_ID: 426,
+  Date: "2022-04-03T00:00:00",
+  Cur_Abbreviation: "CHF",
+  Cur_Scale: 1,
+  Cur_Name: "Швейцарский франк",
+  Cur_OfficialRate: 3.186
+}];
+
+/***/ }),
+
+/***/ "./js/modules/tool_functions.js":
+/*!**************************************!*\
+  !*** ./js/modules/tool_functions.js ***!
+  \**************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "toByn": function() { return /* binding */ toByn; },
+/* harmony export */   "reCalcAll": function() { return /* binding */ reCalcAll; },
+/* harmony export */   "createElement": function() { return /* binding */ createElement; }
+/* harmony export */ });
+
+
+function toByn(currency, _ref) {
+  let {
+    rate,
+    scale
+  } = _ref;
+  return (currency * (rate * scale)).toFixed(2);
+}
+
+function fromByn(byn, _ref2) {
+  let {
+    rate,
+    scale
+  } = _ref2;
+  return (byn / (rate * scale)).toFixed(2);
+}
+
+function reCalcAll(byn, availableInputs, conversionMapBynVal) {
+  availableInputs.forEach(input => {
+    const currencyName = input === null || input === void 0 ? void 0 : input.id;
+    const currencyToByn = conversionMapBynVal[currencyName];
+    input.value = fromByn(byn, currencyToByn);
+  });
+}
+
+function createElement(tagName) {
+  let options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  const el = document.createElement(tagName);
+  Object.entries(options).forEach(_ref3 => {
+    let [optionName, optionValue] = _ref3;
+    el[optionName] = optionValue;
+  });
+  return el;
+}
+
+/***/ }),
+
+/***/ "./index.html":
+/*!********************!*\
+  !*** ./index.html ***!
+  \********************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+// Module
+var code = "<!DOCTYPE html>\r\n<html lang=\"en\">\r\n\r\n<head>\r\n\t<meta charset=\"UTF-8\">\r\n\t<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n\t<title>Convertation</title>\r\n</head>\r\n\r\n <body>\r\n    <div class=\"container__convertor-box\">\r\n      <h1>Конвертор валют</h1>\r\n      <div id=\"message\"></div>\r\n      <a href=\"https://myfin.by/bank/kursy_valjut_nbrb\">Курс НБ РБ</a>\r\n      <div class=\"void__box\">\r\n        <div id=\"inputs\">          \r\n        </div>\r\n      </div>      \r\n    </div>\r\n  </body>\r\n</html>";
+// Exports
+/* harmony default export */ __webpack_exports__["default"] = (code);
+
+/***/ }),
+
+/***/ "./scss/main.scss":
+/*!************************!*\
+  !*** ./scss/main.scss ***!
+  \************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+!function() {
+/*!********************!*\
+  !*** ./js/main.js ***!
+  \********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/main.scss */ "./scss/main.scss");
+/* harmony import */ var _index_html__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../index.html */ "./index.html");
+/* harmony import */ var _modules_old_Cours_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/old_Cours.js */ "./js/modules/old_Cours.js");
+/* harmony import */ var _modules_tool_functions_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/tool_functions.js */ "./js/modules/tool_functions.js");
+
+
+/*
+ * https://www.nbrb.by/apihelp/exrates
+ * Работу с внешним апи я вынес в отдельную функцию,
+ * чтобы можно было протестировать скрипт за пределами Беларуси.
+ *
+ * В случае если апи недоступна - возращается заранее сохраненый ответ эмулирующий работу апи
+ */
+
+
+
+
+function timeOutError(_ref) {
+  let {
+    sec
+  } = _ref;
+  return new Promise((res, rej) => setTimeout(() => rej("Time is out"), sec * 1000));
+}
+
+async function getRatesFromApi() {
+  try {
+    const url = "https://www.nbrb.by/api/exrates/rates?periodicity=0"; // Следующая строка проверяет что запрос не длится дольше 3х секунд
+
+    const response = await Promise.race([fetch(url), timeOutError({
+      sec: 3
+    })]);
+    return await response.json();
+  } catch (e) {
+    console.warn("Error when fetching rates from api. Reason:", e);
+    console.log("Activate fallback mock");
+    document.querySelector("#message").innerHTML = ["Сайт НБРБ не досутпен для вашей страны.", "Будут использованны последние известные данные на 3 апреля 2022 года"].join("\n");
+    return _modules_old_Cours_js__WEBPACK_IMPORTED_MODULE_2__.oldCoursArr;
+  }
+}
+/**
+ * Для удобства я сгруппироввал элементы которые захватываются со страницы
+ * в переменную.
+ * К ним теперь можно обращаться через elements. а IDE уже дальше подскажет
+ * какие есть элементы
+ */
+
+
+const elements = {
+  buttonAdd: document.querySelector(".add-convertor__box"),
+  inputAll: document.querySelector(".void__box"),
+  inputCollection: document.querySelector(".convertor__box"),
+  containerToAdd: document.querySelector("#inputs")
+};
+/**
+ * Эту функцию я сделал "самовызвающейся". По умному это называется
+ * Immediately Invoked Function Expression (IIFE)
+ * https://blog.mgechev.com/2012/08/29/self-invoking-functions-in-javascript-or-immediately-invoked-function-expression/
+ */
+
+(async function (elements) {
+  try {
+    const officialCurrencies = await getRatesFromApi(); // Создаем словарь: валюта -> курс
+
+    const conversionMapBynVal = {};
+    officialCurrencies.forEach(_ref2 => {
+      let {
+        Cur_OfficialRate,
+        Cur_Abbreviation,
+        Cur_Scale
+      } = _ref2;
+      conversionMapBynVal[Cur_Abbreviation] = {
+        rate: Cur_OfficialRate,
+        scale: Cur_Scale
+      };
+    });
+    conversionMapBynVal.BYN = {
+      rate: 1,
+      scale: 1
+    };
+    /**
+     * Для каждой прришедщей из апи валюты
+     * создаем блк с инпутом
+     */
+
+    const availableInputs = officialCurrencies.map(item => {
+      const divConvertorBox = (0,_modules_tool_functions_js__WEBPACK_IMPORTED_MODULE_3__.createElement)("div", {
+        className: "convertor__box row"
+      });
+      const input = (0,_modules_tool_functions_js__WEBPACK_IMPORTED_MODULE_3__.createElement)("input", {
+        type: "number",
+        id: item.Cur_Abbreviation,
+        className: "form-control",
+        value: 0
+      });
+      const divCurrency = (0,_modules_tool_functions_js__WEBPACK_IMPORTED_MODULE_3__.createElement)("div", {
+        className: "currency row"
+      });
+      const createSpanCountryFlag = (0,_modules_tool_functions_js__WEBPACK_IMPORTED_MODULE_3__.createElement)("span", {
+        className: "country-flag flag-usd"
+      });
+      const createSpanValName = (0,_modules_tool_functions_js__WEBPACK_IMPORTED_MODULE_3__.createElement)("span", {
+        className: "val-name",
+        innerText: item.Cur_Abbreviation
+      });
+      elements.containerToAdd.appendChild(divConvertorBox);
+      divConvertorBox.appendChild(input);
+      divConvertorBox.appendChild(divCurrency);
+      divCurrency.appendChild(createSpanCountryFlag);
+      divCurrency.appendChild(createSpanValName);
+      return input;
+    }); // Добавляем слушатель событий с добавленых инпутов
+
+    elements.inputAll.addEventListener("input", e => {
+      const targetInput = e.target;
+      const currencyName = targetInput.id;
+      const currencyValue = targetInput.value;
+      const currencyInByn = (0,_modules_tool_functions_js__WEBPACK_IMPORTED_MODULE_3__.toByn)(currencyValue, conversionMapBynVal[currencyName]);
+      const allInputsExceptCurrent = availableInputs.filter(input => input !== targetInput);
+      (0,_modules_tool_functions_js__WEBPACK_IMPORTED_MODULE_3__.reCalcAll)(currencyInByn, allInputsExceptCurrent, conversionMapBynVal);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})(elements);
+}();
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
